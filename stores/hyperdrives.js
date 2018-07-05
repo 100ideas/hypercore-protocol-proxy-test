8941cf4419b20a5a557ccaf72f02936235827ac8
Glitch (hypercore-protocol-proxy-test) <none>
2018-07-05 20:11:02 +0000

    📮💝 Updated with Glitch

--- stores/hyperdrives.js
+++ stores/hyperdrives.js
@@ -7,15 +7,19 @@ require('events').prototype._maxListeners = 0
 module.exports = store
 
 function store (state, emitter) {
+  state.list = []
+
   const key = '72671c5004d3b956791b6ffca7f05025d62309feaf99cde04c6f434189694291'
   const archive = hyperdrive(ram, key, {sparse: true, sparseMetadata: true})
-  connectToGateway(archive)
+  const cancelConnection = connectToGateway(archive)
   archive.ready(() => {
     archive.metadata.update(() => {
       console.log('Length', archive.metadata.length)
       archive.readdir('/media/content', (err, list) => {
         if (err) throw err
-        console.log('Dir', list)
+        state.list = list
+        emitter.emit('render')
+        cancelConnection()
       })
     })
   })
